1d7bd0390d62363b7f594b745627665ae065267b
txlula <lucynguyen9793@gmail.com>
2026-03-23 11:34:44 +0000

    feat: submit form results

--- src/script.js
+++ src/script.js
@@ -20,3 +20,42 @@ document.getElementById("energyLevel").addEventListener("change", function () {
   const energyLevel = document.getElementById("energyLevel").value;
   document.getElementById("energyLevelValue").textContent = energyLevel;
 });
+
+
+document.getElementById("submitButton").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let score = 0;
+  const mood = document.querySelector('input[name="mood"]:checked').value;
+  const energyLevel = document.getElementById("energyLevel").value;
+
+  if (mood ==="happy") { 
+    score += 50;
+  }
+  else if (mood === "neutral") {
+    score += 25;
+  }
+  else if (mood === "sad") {
+    score += 0;
+  }
+
+  score += (energyLevel / 100) * 50;
+
+  document.getElementById("score").textContent = `Your score: ${score.toFixed(2)}`;
+
+  if (score >= 75) {
+    document.getElementById("resultsContent").textContent = "Great job! You are doing well. Appreciate the work you have done today and keep it up!";
+  }
+  else if (score >= 50 && score < 75) {
+    document.getElementById("resultsContent").textContent = "Not bad! Keep it up. Remember to take breaks and do something you enjoy to boost your mood and energy.";
+  }
+  else {
+    document.getElementById("resultsContent").textContent = "It seems like you're having a tough day. Consider taking a break or doing something you enjoy.";
+  }
+
+  document.getElementById("resultsDialog").showModal();
+});
+
+document.getElementById("closeDialogButton").addEventListener("click", function () {
+  document.getElementById("resultsDialog").close();
+});
